d20aa85585537857fceddd931597ffa8e2d54532
Wilson <wilsonyu657@gmail.com>
2024-06-20 04:56:07 -0400

    Fixed click service

--- Bowling-Sim.lua
+++ Bowling-Sim.lua
@@ -1,4 +1,5 @@
 local replicatedStorage = game:GetService("ReplicatedStorage")
+local runService = game:GetService("RunService")
 
 local lp = game:GetService("Players").LocalPlayer
 
@@ -7,13 +8,11 @@ local Knit = Packages:WaitForChild("Knit")
 
 local KnitServices = Knit:WaitForChild("Services")
 local PlayerResourceService = KnitServices:WaitForChild("PlayerResourceService")
-local ClickService = KnitServices:WaitForChild("PlayerResourceService")
+local ClickService = KnitServices:WaitForChild("ClickService")
 local TrainService = KnitServices:WaitForChild("TrainService")
 
 local ClickRemotes = ClickService:WaitForChild("RF")
 local TrainRemotes = TrainService:WaitForChild("RF")
-
-RF.GetPlayerRankData:InvokeServer(lp)
 
 function sim_click()
     ClickRemotes.Click:InvokeServer()
@@ -22,3 +21,15 @@ end
 function sim_train(number)
     TrainRemotes.PlayerExercise:InvokeServer(number)
 end
+
+function _G.bs_close()
+    if _G.bs_heartbeat then
+        _G.bs_heartbeat:Disconnect()
+        _G.bs_heartbeat = nil
+    end
+end
+
+_G.bs_heartbeat = runService.Heartbeat:Connect(function(delta)
+    sim_click()
+    sim_train(4)
+end)
