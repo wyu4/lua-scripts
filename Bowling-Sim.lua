cef22f723d552973a9cda957df25a03f406dd0e2
Wilson <wilsonyu657@gmail.com>
2024-06-21 18:38:43 -0400

    Added server hopper

--- Bowling-Sim.lua
+++ Bowling-Sim.lua
@@ -14,11 +14,13 @@ local ClickService = KnitServices:WaitForChild("ClickService")
 local TrainService = KnitServices:WaitForChild("TrainService")
 local GameService = KnitServices:WaitForChild("GameService")
 local PetLuckService = KnitServices:WaitForChild("PetLuckService")
+local MapService = KnitServices:WaitForChild("MapService")
 
 local ClickRemotes = ClickService:WaitForChild("RF")
 local TrainRemotes = TrainService:WaitForChild("RF")
 local GameRemoteEvents = GameService:WaitForChild("RE")
 local PetLuckServiceRemotes = PetLuckService:WaitForChild("RF")
+local MapRemotes = MapService:WaitForChild("RF")
 
 function sim_click()
     ClickRemotes.Click:InvokeServer()
@@ -36,6 +38,10 @@ function hatch(egg_num)
     PetLuckServiceRemotes.LuckOnce:InvokeServer(egg_num)
 end
 
+function buy_map(map_num)
+    MapRemotes.BuyMap:InvokeServer(map_num)
+end
+
 function _G.bs_close()
     if _G.bs_heartbeat then
         _G.bs_heartbeat:Disconnect()
@@ -46,6 +52,6 @@ _G.bs_close()
 
 _G.bs_heartbeat = runService.Heartbeat:Connect(function(delta)
     sim_click()
-    sim_train(4)
+    sim_train(7)
     get_pk_money()
 end)
