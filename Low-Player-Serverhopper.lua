c6812c1a245e08b4b9fc71f9ce8339445aa74a6e
Wilson <wilsonyu657@gmail.com>
2024-06-23 03:55:55 -0400

    Added ctrl-h hotkey for teleporting. Run a second time to manualyl teleport

--- Low-Player-Serverhopper.lua
+++ Low-Player-Serverhopper.lua
@@ -1,3 +1,5 @@
+local uis = game:GetService("UserInputService")
+
 local Http = game:GetService("HttpService")
 local TPS = game:GetService("TeleportService")
 local Api = "https://games.roblox.com/v1/games/"
@@ -9,10 +11,53 @@ function ListServers(cursor)
    return Http:JSONDecode(Raw)
 end
 
-local Server, Next; repeat
-   local Servers = ListServers(Next)
-   Server = Servers.data[1]
-   Next = Servers.nextPageCursor
-until Server
+function tp()
+   local Server, Next; repeat
+      local Servers = ListServers(Next)
+      Server = Servers.data[math.random(1,50)]
+      Next = Servers.nextPageCursor
+   until Server
+   
+   TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
+end
 
-TPS:TeleportToPlaceInstance(_place,Server.id,game.Players.LocalPlayer)
+if not _G.LPS_LOADED then
+   _G.LPS_LOADED = true
+
+   local teleporting = false
+
+   local ctrlDown = false
+   local hDown = false
+   uis.InputBegan:Connect(function(input, gameProcessed)
+      if not gameProcessed then
+         if input.KeyCode == Enum.KeyCode.LeftControl then
+            ctrlDown = true
+         elseif input.KeyCode == Enum.KeyCode.H then
+            hDown = true
+         end
+
+         if ctrlDown and hDown then
+            if not teleporting then
+               teleporting = true
+               print("Teleporting...")
+               local success, errormessage = pcall(tp)
+               if not success then
+                  warn(errormessage)
+                  teleporting = false
+               end
+            end
+         end
+      end
+   end)
+
+   uis.InputEnded:Connect(function(input, gameProcessed)
+      if input.KeyCode == Enum.KeyCode.LeftControl then
+         ctrlDown = false
+      elseif input.KeyCode == Enum.KeyCode.H then
+         hDown = false
+      end
+   end)
+
+else
+   tp()
+end
